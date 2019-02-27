797ea5d3d754cb4e759a8601ed5deba7a9c0d7f9
Endy Muhardin <endy.muhardin@gmail.com>
2019-02-27 14:56:40 +0700

    implement layout bootstrap

--- src/main/java/com/muhardin/endy/belajar/selenium/Halo.java
+++ src/main/java/com/muhardin/endy/belajar/selenium/Halo.java
@@ -1,12 +1,20 @@
 package com.muhardin.endy.belajar.selenium;
 
+import nz.net.ultraq.thymeleaf.LayoutDialect;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class Halo {
+
     public static void main(String[] args) {
         System.out.println("Halo");
         SpringApplication.run(Halo.class, args);
+    }
+
+    @Bean
+    public LayoutDialect layoutDialect() {
+        return new LayoutDialect();
     }
 }
---
+++ src/main/java/com/muhardin/endy/belajar/selenium/controller/CustomerController.java
@@ -0,0 +1,16 @@
+package com.muhardin.endy.belajar.selenium.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/customer")
+public class CustomerController {
+    
+    @GetMapping("/list")
+    public void dataCustomer(){
+        
+    }
+    
+}
