eca01e7d4d6d979abbd437e7cfe780de2ac6dd71
Endy Muhardin <endy.muhardin@gmail.com>
2019-02-28 13:38:33 +0700

    implement validasi input

--- src/main/java/com/muhardin/endy/belajar/selenium/controller/CustomerController.java
+++ src/main/java/com/muhardin/endy/belajar/selenium/controller/CustomerController.java
@@ -2,14 +2,17 @@ package com.muhardin.endy.belajar.selenium.controller;
 
 import com.muhardin.endy.belajar.selenium.dao.CustomerDao;
 import com.muhardin.endy.belajar.selenium.entity.Customer;
+import javax.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.support.SessionStatus;
 
 @Controller
 @RequestMapping("/customer")
@@ -28,9 +31,15 @@ public class CustomerController {
     }
     
     @PostMapping("/form")
-    public String prosesFormEdit(@ModelAttribute Customer customer){
+    public String prosesFormEdit(@ModelAttribute @Valid Customer customer, BindingResult errors, SessionStatus status){
         System.out.println("Birthdate " + customer.getBirthdate());
+        
+        if(errors.hasErrors()){
+            return "/customer/form";
+        }
+        
         customerDao.save(customer);
+        status.setComplete();
         return "redirect:list";
     }
     
--- src/main/java/com/muhardin/endy/belajar/selenium/entity/Customer.java
+++ src/main/java/com/muhardin/endy/belajar/selenium/entity/Customer.java
@@ -6,6 +6,10 @@ import javax.persistence.EnumType;
 import javax.persistence.Enumerated;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
 import lombok.Data;
 import org.hibernate.annotations.GenericGenerator;
 
@@ -15,15 +19,24 @@ public class Customer {
     @Id @GeneratedValue(generator = "uuid")
     @GenericGenerator(name = "uuid", strategy = "uuid2")
     private String id;
+    
+    @NotEmpty @Size(min = 3)
     private String fullname;
+    
+    @NotEmpty @Size(min = 6) @Email
     private String email;
+    
+    @NotEmpty @Size(min = 5, max = 13)
     private String mobilePhone;
     
+    @NotNull
     private LocalDate birthdate;
     
+    @NotNull
     @Enumerated(EnumType.STRING)
     private Gender gender; 
     
+    @NotNull
     @Enumerated(EnumType.STRING)
     private Education education;
 }
