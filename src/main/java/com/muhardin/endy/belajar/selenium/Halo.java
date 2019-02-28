f76cde02cba19c92cf83b41886d932325577a58c
Endy Muhardin <endy.muhardin@gmail.com>
2019-02-28 10:38:46 +0700

    implement list

--- src/main/java/com/muhardin/endy/belajar/selenium/Halo.java
+++ src/main/java/com/muhardin/endy/belajar/selenium/Halo.java
@@ -4,6 +4,7 @@ import nz.net.ultraq.thymeleaf.LayoutDialect;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.thymeleaf.dialect.springdata.SpringDataDialect;
 
 @SpringBootApplication
 public class Halo {
@@ -16,5 +17,10 @@ public class Halo {
     @Bean
     public LayoutDialect layoutDialect() {
         return new LayoutDialect();
+    }
+    
+    @Bean
+    public SpringDataDialect springDataDialect() {
+        return new SpringDataDialect();
     }
 }
